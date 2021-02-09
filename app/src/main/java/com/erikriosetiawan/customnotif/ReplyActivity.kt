47098cd57ad2ae475e8514f4d921cc8c2543334d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-09 11:25:15 +0700

    Create ReplyActivity.kt

---
+++ app/src/main/java/com/erikriosetiawan/customnotif/ReplyActivity.kt
@@ -0,0 +1,11 @@
+package com.erikriosetiawan.customnotif
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReplyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reply)
+    }
+}
