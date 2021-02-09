0997b57f8cfec1c0cd377e45af1580dbd5f24a81
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-09 11:52:03 +0700

    Add a method to update notification

--- app/src/main/java/com/erikriosetiawan/customnotif/ReplyActivity.kt
+++ app/src/main/java/com/erikriosetiawan/customnotif/ReplyActivity.kt
@@ -1,11 +1,82 @@
 package com.erikriosetiawan.customnotif
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import com.erikriosetiawan.customnotif.NotificationService.Companion.CHANNEL_ID
+import com.erikriosetiawan.customnotif.NotificationService.Companion.CHANNEL_NAME
+import com.erikriosetiawan.customnotif.NotificationService.Companion.REPLY_ACTION
+import com.erikriosetiawan.customnotif.databinding.ActivityReplyBinding
 
 class ReplyActivity : AppCompatActivity() {
+
+    companion object {
+        private const val KEY_MESSAGE_ID = "key_message_id"
+        private const val KEY_NOTIFY_ID = "key_notify_id"
+
+        fun getReplyMessageIntent(context: Context, notifyId: Int, messageId: Int): Intent {
+            val intent = Intent(context, ReplyActivity::class.java)
+            intent.action = REPLY_ACTION
+            intent.putExtra(KEY_MESSAGE_ID, messageId)
+            intent.putExtra(KEY_NOTIFY_ID, notifyId)
+            return intent
+        }
+    }
+
+    private var mMessageId: Int = 0
+    private var mNotifyId: Int = 0
+    private lateinit var binding: ActivityReplyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reply)
+        binding = ActivityReplyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = intent
+        if (REPLY_ACTION == intent.action) {
+            mMessageId = intent.getIntExtra(KEY_MESSAGE_ID, 0)
+            mNotifyId = intent.getIntExtra(KEY_NOTIFY_ID, 0)
+        }
+
+        binding.buttonSend.setOnClickListener { sendMessage(mNotifyId, mMessageId) }
+    }
+
+    private fun sendMessage(notifyId: Int, messageId: Int) {
+        updateNotification(notifyId)
+
+        val message = binding.editReply.text.toString().trim { it <= ' ' }
+        Toast.makeText(this, "Message ID: $messageId\nMessage: $message", Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
+    private fun updateNotification(notifyId: Int) {
+        val notificationManagerCompat =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+            .setContentTitle(getString(R.string.notif_title_sent))
+            .setContentText(getString(R.string.notif_content_sent))
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(1000, 1000, 1000, 1000, 1000)
+
+            builder.setChannelId(CHANNEL_ID)
+            notificationManagerCompat.createNotificationChannel(channel)
+        }
+
+        val notification = builder.build()
+        notificationManagerCompat.notify(notifyId, notification)
     }
 }
